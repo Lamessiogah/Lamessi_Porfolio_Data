9672d8128e028818365ff1da7640673ccbb22f31
Lamessiogah <lamessijerome@gmail.com>
2026-05-04 22:26:03 +0200

    Update RK4.m

--- RK4.m
+++ RK4.m
@@ -1,12 +1,21 @@
-function [v] = RK4(v0,t,f)
- v = [ v0 ];
- h = t(2)-t(1);
- for tn = t(1:(end-1))
-  vn = v( :, end);
-  k1 = f(tn,vn);
-  k2 = f(tn + h/2, vn + h*k1/2);
-  k3 = f(tn + h/2, vn + h*k2/2);
-  k4 = f(tn + h  , vn + h*k3);
-  v = [ v , vn+h*(k1+2*k2+2*k3+k4)/6 ];
- end
+function v = RK4(v0, t, f)
+
+  h = t(2) - t(1);
+  n = length(t);
+
+  v = zeros(length(v0), n); % préallocation
+  v(:,1) = v0;
+
+  for i = 1:n-1
+    tn = t(i);
+    vn = v(:,i);
+
+    k1 = f(tn, vn);
+    k2 = f(tn + h/2, vn + h*k1/2);
+    k3 = f(tn + h/2, vn + h*k2/2);
+    k4 = f(tn + h,   vn + h*k3);
+
+    v(:,i+1) = vn + h*(k1 + 2*k2 + 2*k3 + k4)/6;
+  end
+
 endfunction
